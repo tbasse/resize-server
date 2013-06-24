71422d3d4923870acbba724e9d616164635f308a
tbasse <himself@tagedieb.com>
2013-06-24 21:46:04 +0200

    updates helper.js tests

--- test/helper.js
+++ test/helper.js
@@ -10,14 +10,19 @@ describe('getFileSuffix', function () {
     expect(helper.getFileSuffix).to.be.an.instanceOf(Function);
   });
 
-  it('returns correct suffix', function () {
+  it('returns correct suffix on one dot filenames', function () {
     var str = 'this_is_a.jpg';
     expect(helper.getFileSuffix(str)).to.equal('jpg');
   });
 
-  it('returns correct suffix', function () {
+  it('returns correct suffix on multiple dot filenames', function () {
     var str = 'this_is_a.jpg.png';
     expect(helper.getFileSuffix(str)).to.equal('png');
+  });
+
+  it('returns correct suffix on none dot filenames', function () {
+    var str = 'this_has_no_suffix';
+    expect(helper.getFileSuffix(str)).to.equal('');
   });
 
 });
