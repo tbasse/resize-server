095cbd1b20e3dc397f478714e5e0f6bdc65fd408
tbasse <himself@tagedieb.com>
2013-07-23 12:00:05 +0200

    add grunt watch task

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,16 +5,35 @@ module.exports = function (grunt) {
     mochaTest: {
       test: {
         options: {
-          reporter: 'dot'
+          reporter: 'spec'
         },
         src: ['spec/**/*.js']
+      }
+    },
+    clear: {
+      tasks: ['clear']
+    },
+    watch: {
+      test: {
+        files: [
+          'server.js',
+          'lib/**/*.js',
+          'spec/**/*.js'
+        ],
+        tasks: [
+          'clear',
+          'mochaTest'
+        ]
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-clear');
 
   grunt.registerTask('default', [
+    'clear',
     'mochaTest'
   ]);
 
